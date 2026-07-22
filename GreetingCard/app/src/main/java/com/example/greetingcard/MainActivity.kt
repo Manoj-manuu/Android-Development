08421db0b6344f211ecbc23ef95a1b9b4663c5e4
manoj <manojchitrgar@gmail.com>
2026-07-23 02:21:43 +0530

    Initialize Android Development repository

---
+++ GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.greetingcard
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.greetingcard.ui.theme.GreetingCardTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GreetingCardTheme {
+                Greeting(name = "Manoj")
+                }
+            }
+        }
+    }
+
+
+@Composable
+fun Greeting(name: String) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Surface(color = Color.Green) {
+            Text(
+                text = "Hi, My name is $name!",
+                modifier = Modifier.padding(24.dp)
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = false)
+@Composable
+fun GreetingPreview() {
+    GreetingCardTheme {
+        Greeting("Manoj")
+    }
+}
